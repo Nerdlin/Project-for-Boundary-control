0465de5f4751340e36eb0dad83db1e4a286dd894
Nerdlin <nerdlinnut@gmail.com>
2025-04-01 12:42:52 +0500

    Обновление

--- datascience_project/datascience_project/settings.py
+++ datascience_project/datascience_project/settings.py
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
@@ -130,6 +130,15 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 LOGIN_URL = '/auth/login/'  # URL для перенаправления неавторизованных пользователей
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+# Email settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'your_email@gmail.com'
+EMAIL_HOST_PASSWORD = 'your_password'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
--- datascience_project/pages_app/views.py
+++ datascience_project/pages_app/views.py
@@ -1,31 +1,47 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 # Create your views here.
 from django.shortcuts import render, redirect
 from .models import GalleryImage
 from .forms import GalleryImageForm
 
+@login_required(login_url='login')  # Перенаправляет на страницу входа, если не авторизован
 def index_view(request):
     return render(request, 'index.html')
 
+@login_required(login_url='login')  # Перенаправляет на страницу входа, если не авторизован
 def history_view(request):
     return render(request, 'history.html')
 
+@login_required(login_url='login')  # Перенаправляет на страницу входа, если не авторизован
 def features_view(request):
     return render(request, 'features.html')
 
+@login_required(login_url='login')  # Перенаправляет на страницу входа, если не авторизован
 def skills_view(request):
     return render(request, 'skills.html')
 
 
 @login_required(login_url='login')  # Перенаправляет на страницу входа, если не авторизован
 def gallery_view(request):
-    if request.method == 'POST':
-        form = GalleryImageForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('gallery')
-    else:
-        form = GalleryImageForm()
-
+    form = GalleryImageForm()  # Инициализируем форму по умолчанию
     images = GalleryImage.objects.all()
+
+    if request.method == 'POST':
+        if 'delete_selected' in request.POST:
+            selected_ids = request.POST.getlist('delete_images')
+            GalleryImage.objects.filter(id__in=selected_ids).delete()
+            messages.success(request, "Выбранные изображения удалены.")
+        elif 'delete_all' in request.POST:
+            GalleryImage.objects.all().delete()
+            messages.success(request, "Все изображения удалены.")
+        else:
+            form = GalleryImageForm(request.POST, request.FILES)
+            if form.is_valid():
+                form.save()
+                messages.success(request, "Изображение успешно загружено.")
+            else:
+                messages.error(request, "Ошибка при загрузке изображения.")
+        return redirect('gallery')
+
     return render(request, 'gallery.html', {'images': images, 'form': form})
